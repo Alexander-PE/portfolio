95776b056f530a27684ef7f0894e17fa7acda845
Alexander Perez <alexander.pereze02@gmail.com>
2023-01-07 14:00:40 -0500

    Update: Header UX

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { SocialIcon } from 'react-social-icons'
 
 type Props = {}
 
+const left = {
+    init: { x: -500, opacity: 0, scale: 0.5 },
+    anim: { x: 0, opacity: 1, scale: 1},
+    trans: { duration: 1.5 }
+}
+
+const right = {
+    init: { x: 500, opacity: 0, scale: 0.5 },
+    anim: { x: 0, opacity: 1, scale: 1},
+    trans: { duration: 1.5 }
+}
+
 export const Header = (props: Props) => {
   return (
-    <div>Header</div>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+        <motion.div initial={left.init} animate={left.anim} transition={left.trans} className='flex flex-row items-center'>
+            <SocialIcon url="https://github.com/Alexander-PE" fgColor='gray' bgColor='transparent' />
+            <SocialIcon url="https://www.linkedin.com/in/alexander-perez-encarnacion-b822821b5/" fgColor='gray' bgColor='transparent' />
+        </motion.div>
+
+        <motion.div initial={right.init} animate={right.anim} transition={right.trans} className='flex flex-row items-center text-gray-300'>
+            <SocialIcon className='cursor-pointer' network='email' fgColor='gray' bgColor='transparent' />
+            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>get in touch</p>
+        </motion.div>
+    </header>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import Head from 'next/head'
+import { Header } from '../components/Header'
 
 export default function Home() {
   return (
-    <>
+    <div className='text-white'>
       <Head>
         <title>Alexander's Portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
+      <Header/>
 
-    </>
+
+
+    </div>
   )
-}
+} 
