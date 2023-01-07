838bbb72d079219512f0e47017c7cf69740ba9e2
Alexander Perez <alexander.pereze02@gmail.com>
2023-01-07 15:42:17 -0500

    Update: index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Hero } from '../components/Hero'
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen overflow-x-hidden'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen overflow-x-hidden snap-y snap-mandatory overflow-y-scroll z-0'>
       <Head>
         <title>Alexander's Portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -15,7 +15,6 @@ export default function Home() {
       <Hero />
 
  
-
     </div>
   )
 } 
