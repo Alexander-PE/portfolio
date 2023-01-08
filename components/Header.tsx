a39cd65b29bc7721497062d66f77056fc460b070
Alexander Perez <alexander.pereze02@gmail.com>
2023-01-08 00:03:58 -0500

    Update: add twitter icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,12 +19,13 @@ export const Header = (props: Props) => {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
         <motion.div initial={left.init} animate={left.anim} transition={left.trans} className='flex flex-row items-center'>
-            <SocialIcon url="https://github.com/Alexander-PE" fgColor='gray' bgColor='transparent' />
-            <SocialIcon url="https://www.linkedin.com/in/alexander-perez-encarnacion-b822821b5/" fgColor='gray' bgColor='transparent' />
+            <SocialIcon url='https://twitter.com/_apwrld' className='socialIconsHover' fgColor='gray' bgColor='transparent'/>
+            <SocialIcon url="https://github.com/Alexander-PE" className='socialIconsHover' fgColor='gray' bgColor='transparent' />
+            <SocialIcon url="https://www.linkedin.com/in/alexander-perez-encarnacion-b822821b5/" className='socialIconsHover' fgColor='gray' bgColor='transparent' />
         </motion.div>
 
         <motion.div initial={right.init} animate={right.anim} transition={right.trans} className='flex flex-row items-center text-gray-300'>
-            <SocialIcon className='cursor-pointer' url='mailto:alexander.pereze02@gmail.com' network='email' fgColor='gray' bgColor='transparent' />
+            <SocialIcon className='socialIconsHover' url='mailto:alexander.pereze02@gmail.com' network='email' fgColor='gray' bgColor='transparent' />
             <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>get in touch</p>
         </motion.div>
     </header>
