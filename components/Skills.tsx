40a34fdb24d126f2e3510effa5c6ae292696da2d
AlexanderPerez <alexander.pereze02@gmail.com>
2023-02-02 02:37:26 -0500

    Update: Text

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,7 +13,7 @@ export const Skills = () => {
             <motion.div className="skills" initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1.5}}>
                 <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">skills</h3>
 
-                <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skill for information</h3>
+                {/* <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skill for information</h3> */}
             
                 <div className="grid grid-cols-4 gap-7">
                     <HTML />
